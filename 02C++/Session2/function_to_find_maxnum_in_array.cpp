a1d367a127fab39969e26384433aa14db72783ab
som3a13 <134228578+som3a13@users.noreply.github.com>
2023-10-16 21:53:41 +0300

    Update function_to_find_maxnum_in_array.cpp

--- 02C++/Session2/function_to_find_maxnum_in_array.cpp
+++ 02C++/Session2/function_to_find_maxnum_in_array.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <iterator>
 int maxNumArray(int * arr,int size);
+void max();
 int main(){
    
     int arr[]={10,20,40,15,17,18,19};
     int size = sizeof(arr)/sizeof(arr[0]);
 std::cout << "MAX element =" <<     maxNumArray(arr,size)<<std::endl;    
+max();
 return 0;
     
 }
@@ -26,3 +28,10 @@ return max;
 
 }
 
+void max(){
+     int arr[]={10,20,99,15,17,18,19};
+     
+     auto max =std::max_element(std::begin(arr),std::end(arr));
+
+    std::cout << *max << std::endl;
+}
